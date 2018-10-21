1a325a90bcec138205bbe28c97c10240933032c3
aloranking <adigunlateef02@gmail.com>
2018-10-21 22:50:14 +0100

    the layout is created

--- app/src/main/java/com/aloranking/plantdiseasesdetector/MainActivity.java
+++ app/src/main/java/com/aloranking/plantdiseasesdetector/MainActivity.java
@@ -1,13 +1,148 @@
 package com.aloranking.plantdiseasesdetector;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+    private String mTempPhotoPath;
+    private Bitmap mResultsBitmap;
+
+    private ImageView imageView;
+    private FloatingActionButton mShareFab;
+    private FloatingActionButton mSaveFab;
+    private FloatingActionButton mClearFab;
+    private Button mDetectButton;
+
+    private TextView mWelcomeText;
+    private TextView mInfoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        mShareFab = findViewById(R.id.share_button);
+        mSaveFab = findViewById(R.id.save_button);
+        mClearFab = findViewById(R.id.clear_button);
+        mDetectButton = findViewById(R.id.detect_button);
+
+        mWelcomeText = findViewById(R.id.welcome_text);
+        mInfoText = findViewById(R.id.info_about_disease);
+    }
+
+
+    /**
+     * OnClick method for "Detect Plant" Button. Launches the camera app.
+     *
+     * @param view The Detect Plant button.
+     */
+    public void DetectPlant(View view) {
+        // Check for the external storage permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If you do not have permission, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Launch the camera if the permission exists
+            launchCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Called when you request permission to read and write to external storage
+        switch (requestCode) {
+            case REQUEST_STORAGE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // If you get permission, launch the camera
+                    launchCamera();
+                } else {
+                    // If you do not get permission, show a Toast
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+
+
+    /**
+     * Creates a temporary image file and captures a picture to store in it.
+     */
+    private void launchCamera() {
+
+        // Create the capture image intent
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the image capture activity was called and was successful
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+
+            // Otherwise, delete the temporary image file
+            BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+
+    private void processAndSetImage() {
+
+        mWelcomeText.setVisibility(View.GONE);
+        mDetectButton.setVisibility(View.GONE);
+
+        mSaveFab.setVisibility(View.VISIBLE);
+        mShareFab.setVisibility(View.VISIBLE);
+        mClearFab.setVisibility(View.VISIBLE);
+        mInfoText.setVisibility(View.VISIBLE);
+
+
+        mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
+        imageView.setImageBitmap(mResultsBitmap);
+
+    }
+
+    public void shareImage(View view) {
+    }
+
+    public void clearImage(View view) {
+    }
+
+    public void saveImage(View view) {
     }
 }
